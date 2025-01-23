36ef5fcf0147513bf8379a54353d26b38eba89e2
kerolos Magdy <kero.magdy2000@gmail.com>
2025-01-23 22:55:53 +0200

    add locale folder for en and ar

---
+++ src/locale/ar.ts
@@ -0,0 +1,31 @@
+export const ar = {
+  mode: {
+    dark: 'الوضع الداكن',
+    light: 'الوضع الفاتح',
+  },
+  home: {
+    hero: {
+      headline: {
+        improve: 'طور',
+        skill: 'مهاراتك',
+        withDifferentWay: 'بطريقة مختلفة',
+      },
+      description:
+        'دعنا نأخذ دورة تدريبية عبر الإنترنت لتحسين مهاراتك بطريقة مختلفة. يمكنك تحديد وقت الدراسة الخاص بك وفقًا لسرعتك في التعلم، حتى تتمكن من الدراسة براحة وامتصاص المادة بسهولة.',
+      buttons: {
+        getStarted: 'ابدأ الآن',
+        watchVideo: 'شاهد الفيديو',
+      },
+      certificate: {
+        title: 'شهادة',
+        description: 'تتوفر شهادات لجميع الدورات.',
+      },
+      experience: {
+        students: 'طلاب',
+        qualityCourse: 'دورات عالية الجودة',
+        experienceMentors: 'مرشدون بخبرة',
+      },
+    },
+    feature: {},
+  },
+}
---
+++ src/locale/en.ts
@@ -0,0 +1,43 @@
+export const en = {
+  mode: {
+    dark: 'Dark Mode',
+    light: 'Light Mode',
+  },
+  home: {
+    hero: {
+      headline: {
+        improve: 'Improve',
+        skill: 'Skill',
+        withDifferentWay: 'with Different Way',
+      },
+      description:
+        "Let's take an online course to improve your skills in a different way, you can set your own study time according to your learning speed. So you can study comfortably and absorb the material easily.",
+      buttons: {
+        getStarted: 'Get Started',
+        watchVideo: 'Watch Video',
+      },
+      certificate: {
+        title: 'Certificate',
+        description: 'There are certificates for all courses.',
+      },
+      experience: {
+        students: 'Students',
+        qualityCourse: 'Quality Course',
+        experienceMentors: 'Experience Mentors',
+      },
+    },
+    feature: {
+      sectionTitle: 'Features',
+      designUiUx: 'UI/UX Design',
+      mobileDev: 'Mobile Development',
+      webDev: 'Web Development',
+      statsTitle: 'Statistics',
+      statsSubtitle: 'There are many variations of passages of Lorem Ipsum available',
+      headlinePart1: 'Make your',
+      headlinePart2: 'Learning',
+      headlinePart3: 'Enjoyable',
+      description:
+        ' Set the way of learning according to your wishes with some of the benefits that you get us, so you on enjoy the lessons that we provide.',
+    },
+  },
+}
