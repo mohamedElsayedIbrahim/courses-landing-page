930f166c166f177e6be967b24fe9e4a707669017
kerolos Magdy <kero.magdy2000@gmail.com>
2025-01-23 16:07:49 +0200

    fix navigations section in footer and header

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,60 +1,55 @@
-import React, { FC } from 'react'
 import Box from '@mui/material/Box'
-import { Link as ScrollLink } from 'react-scroll'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
 import { navigations } from './navigation.data'
 
 const Navigation: FC = () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' } }}>
       {navigations.map(({ path: destination, label }) => (
-        <Box
-          component={ScrollLink}
-          key={destination}
-          activeClass="current"
-          to={destination}
-          spy={true}
-          smooth={true}
-          duration={350}
-          sx={{
-            position: 'relative',
-            color: 'text.disabled',
-            cursor: 'pointer',
-            fontWeight: 600,
-            display: 'inline-flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            px: { xs: 0, md: 3 },
-            mb: { xs: 3, md: 0 },
-            fontSize: { xs: '1.2rem', md: 'inherit' },
-            ...(destination === '/' && {
-              color: 'primary.main',
-            }),
-
-            '& > div': { display: 'none' },
-
-            '&.current>div': { display: 'block' },
-
-            '&:hover': {
-              color: 'primary.main',
-              '&>div': {
-                display: 'block',
-              },
-            },
-          }}
-        >
+        <Link key={destination} href={destination} passHref>
           <Box
             sx={{
-              position: 'absolute',
-              top: 12,
-              transform: 'rotate(3deg)',
-              '& img': { width: 44, height: 'auto' },
+              position: 'relative',
+              color: 'text.disabled',
+              cursor: 'pointer',
+              fontWeight: 600,
+              display: 'inline-flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              px: { xs: 0, md: 3 },
+              mb: { xs: 3, md: 0 },
+              fontSize: { xs: '1.2rem', md: 'inherit' },
+              ...(destination === '/' && {
+                color: 'primary.main',
+              }),
+
+              '& > div': { display: 'none' },
+
+              '&.current>div': { display: 'block' },
+
+              '&:hover': {
+                color: 'primary.main',
+                '&>div': {
+                  display: 'block',
+                },
+              },
             }}
           >
-            {/* eslint-disable-next-line */}
-            <img src="/images/headline-curve.svg" alt="Headline curve" />
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 12,
+                transform: 'rotate(3deg)',
+                '& img': { width: 44, height: 'auto' },
+              }}
+            >
+              <Image height={44} width={44} src="/images/headline-curve.svg" alt="Headline curve" />
+            </Box>
+            {label}
           </Box>
-          {label}
-        </Box>
+        </Link>
       ))}
     </Box>
   )
